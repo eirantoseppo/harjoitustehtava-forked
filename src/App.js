563123bfe93d9e8d1bceac596e1e53b62d83d758
Seppo Eiranto <eirantoseppo@gmail.com>
2022-12-07 18:17:01 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './styles.css';
+
+/**
+ *  ///// KÄYTTÄJIEN HALLINTAPANEELI /////
+ *
+ *
+ * - Hae lista käyttäjistä rajapinnasta: https://jsonplaceholder.typicode.com/users
+ *
+ * Ominaisuudet:
+ *
+ * - Käyttäjien listaus
+ * - Uuden käyttäjän lisäys lomakkeella
+ * - Käyttäjän poisto
+ * - Käyttöliittymän ei tarvitse olla graafisesti näyttävä
+ *
+ * Tehtävää tehdessä on tärkeää miettiä miten toiminnallisuuksien tulisi toimia hyvin suunnitellussa sovelluksessa.
+ * Tärkeintä ei ole se että kaikki ominaisuudet on toteutettu vaan se että toteutus toimii ja on laadukas.
+ *
+ * Huom! Käyttäjät haetaan rajapinnasta, mutta sen lisäksi ei ole tarvetta tehdä api kutsuja
+ *
+ * Kun olet tehnyt tehtävän, palauta linkki siihen sähköpostilla osoitteeseen: juhana.jauhiainen@codemen.fi. Linkin saa oikeasta yläkulmasta (Share->Copy Sandbox link)
+ *
+ */
+
+export default function App() {
+    return <div className="App"></div>;
+}
