88ebb274e96a4ce9844e5f7848ddf8fd32d2e3c5
Seppo Eiranto <eirantoseppo@gmail.com>
2022-12-07 22:54:22 +0200

    Add edit function for user

--- src/UserForm.js
+++ src/UserForm.js
@@ -2,54 +2,66 @@ import { Button, TextField } from '@mui/material';
 import React from 'react';
 import { useState } from 'react';
 
-export default function UserCreate({ addUser }) {
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [website, setWebsite] = useState('');
+export default function UserForm({ user, handleUser }) {
+  const [name, setName] = useState(user?.name || '');
+  const [username, setUsername] = useState(user?.username || '');
+  const [email, setEmail] = useState(user?.email || '');
+  const [phone, setPhone] = useState(user?.phone || '');
+  const [website, setWebsite] = useState(user?.website || '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addUser({ name, username, email, phone, website });
+    handleUser({
+      ...user,
+      name,
+      username,
+      email,
+      phone,
+      website,
+    });
   };
 
   return (
     <div>
-      <h1>Create new user</h1>
+      <h1>User</h1>
       <form id="userForm" onSubmit={handleSubmit}>
         <TextField
           id="outlined-basic"
           label="Name"
           variant="outlined"
+          value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Username"
           variant="outlined"
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Email"
           variant="outlined"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Phone"
           variant="outlined"
+          value={phone}
           onChange={(e) => setPhone(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Website"
           variant="outlined"
+          value={website}
           onChange={(e) => setWebsite(e.target.value)}
         />
         <Button variant="contained" type="submit">
-          Create
+          Save
         </Button>
       </form>
     </div>
--- src/UserFrame.js
+++ src/UserFrame.js
@@ -9,15 +9,31 @@ import { Button } from '@mui/material';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
-import UserCreate from './UserCreate';
+import UserForm from './UserForm';
 
 export default function UserFrame() {
   const [users, setUsers] = useState([]);
   const [createUser, setCreateUser] = useState(false);
+  const [editUser, setEditUser] = useState(null);
 
-  const addUser = (user) => {
-    setUsers([user, ...users]);
-    setCreateUser(false);
+  /**
+   * Create or update user in users list based on weither user has id or not
+   * @param {User} user
+   */
+  const handleUsersModifications = (user) => {
+    // If user has id, update user
+    if (user.id) {
+      const newUsers = users.map((u) => (u.id === user.id ? user : u));
+      setUsers(newUsers);
+      setEditUser(null);
+      setCreateUser(false);
+    } else {
+      // Add new user
+      user.id = users.reduce((max, user) => Math.max(max, user.id), 0) + 1;
+      setUsers([user, ...users]);
+      setEditUser(null);
+      setCreateUser(false);
+    }
   };
 
   useEffect(() => {
@@ -47,6 +63,7 @@ export default function UserFrame() {
                 <TableCell align="right">Email</TableCell>
                 <TableCell align="right">Phone</TableCell>
                 <TableCell align="right">Website</TableCell>
+                <TableCell align="center">Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -59,15 +76,20 @@ export default function UserFrame() {
                   <TableCell align="right">{user.email}</TableCell>
                   <TableCell align="right">{user.phone}</TableCell>
                   <TableCell align="right">{user.website}</TableCell>
+                  <TableCell align="right">
+                    <Button variant="contained" onClick={() => setEditUser(user)}>
+                      Edit
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
       </div>
-      {createUser ? (
+      {createUser || editUser ? (
         <div>
-          <UserCreate addUser={addUser} />
+          <UserForm user={editUser} handleUser={handleUsersModifications} />
         </div>
       ) : null}
     </div>
