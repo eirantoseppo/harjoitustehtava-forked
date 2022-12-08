703a586f75edc7650c598c790d3fbb4883da670e
Seppo Eiranto <eirantoseppo@gmail.com>
2022-12-08 19:12:43 +0200

    Add some validation to user form

--- src/UserForm.js
+++ src/UserForm.js
@@ -8,9 +8,14 @@ export default function UserForm({ user, handleUser }) {
   const [email, setEmail] = useState(user?.email || '');
   const [phone, setPhone] = useState(user?.phone || '');
   const [website, setWebsite] = useState(user?.website || '');
+  const [error, setError] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+
     handleUser({
       ...user,
       name,
@@ -21,6 +26,70 @@ export default function UserForm({ user, handleUser }) {
     });
   };
 
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = {};
+    if (validateName(name)) {
+      newErrors.name = 'Name is required';
+      valid = false;
+    }
+    if (validateUsername(username)) {
+      newErrors.username = 'Username must be at least 3 characters';
+      valid = false;
+    }
+    if (!validateEmail(email)) {
+      newErrors.email = 'Enter a valid email address';
+      valid = false;
+    }
+    setError({
+      ...error,
+      ...newErrors,
+    });
+    return valid;
+  };
+
+  const handleName = (name) => {
+    if (validateName(name)) {
+      setError({ ...error, name: 'Name is required' });
+    } else {
+      delete error.name;
+      setError({ ...error });
+    }
+    setName(name);
+  };
+
+  const handleUsername = (username) => {
+    if (validateUsername(username)) {
+      setError({
+        ...error,
+        username: 'Username must be at least 3 characters',
+      });
+    } else {
+      delete error.username;
+      setError({ ...error });
+    }
+    setUsername(username);
+  };
+
+  const handleEmail = (email) => {
+    if (!validateEmail(email)) {
+      setError({
+        ...error,
+        email: 'Enter a valid email address',
+      });
+    } else {
+      delete error.email;
+      setError({ ...error });
+    }
+    setEmail(email);
+  };
+
+  const handlePhone = (phone) => {
+    // Handle phone number validation with isValidPhoneNumber from libphonenumber-js
+    // when you can access user location
+    setPhone(phone);
+  };
+
   return (
     <div>
       <h1>User</h1>
@@ -29,29 +98,37 @@ export default function UserForm({ user, handleUser }) {
           id="outlined-basic"
           label="Name"
           variant="outlined"
+          error={!!error.name}
+          helperText={error.name}
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => handleName(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Username"
           variant="outlined"
+          error={!!error.username}
+          helperText={error.username}
           value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => handleUsername(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Email"
           variant="outlined"
+          error={!!error.email}
+          helperText={error.email}
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => handleEmail(e.target.value)}
         />
         <TextField
           id="outlined-basic"
           label="Phone"
           variant="outlined"
+          error={!!error.phone}
+          helperText={error.phone}
           value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          onChange={(e) => handlePhone(e.target.value)}
         />
         <TextField
           id="outlined-basic"
@@ -67,3 +144,18 @@ export default function UserForm({ user, handleUser }) {
     </div>
   );
 }
+
+const validateName = (name) => {
+  return !name;
+};
+
+const validateUsername = (username) => {
+  return username?.length < 3;
+};
+
+// Check if email at least has a valid format
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
